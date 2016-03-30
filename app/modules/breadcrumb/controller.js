ce18a5185ad8a5289e50669d57eefd2e3c49fa6b
David Schkalee <david.schkalee@sysvision.de>
2016-03-30 13:02:00 +0200

    Cache title Optimized breadcrumb rendering

--- app/modules/breadcrumb/controller.js
+++ app/modules/breadcrumb/controller.js
@@ -1,8 +1,9 @@
 import * as Marionette from 'backbone.marionette'
 import $ from 'jquery'
-import {Collection, history} from 'backbone'
+import {Collection} from 'backbone'
 import BreadcrumbView from './views/Breadcrumb'
 import Config from '../../Config'
+import {sessionStorage} from '../../utils'
 
 class BreadcrumbController extends Marionette.Object {
     init(region) {
@@ -11,35 +12,48 @@ class BreadcrumbController extends Marionette.Object {
 
     initPlaylists() {
         this._currentPlaylistId = null;
-        
-        this._region.show(
-            new BreadcrumbView({
-                collection: new Collection([
-                    { title: 'Playlists' }
-                ])
-            })
-        );
+
+        this._showBreadcrumb([
+            { title: 'Playlists' }
+        ]);
     }
 
     initPlaylist(playlistId) {
-        if(this._currentPlaylistId === playlistId) return;
+        // Cache
+        var title = sessionStorage.get(`${playlistId}.info`, 'title');
+
+        if (title) {
+            return this._showBreadcrumb([
+                { title: 'Playlists', route: 'playlists' },
+                { title }
+            ]);
+        }
+
+        if (this._currentPlaylistId === playlistId) return;
 
         // Retrieve playlist-name
         $.get(`${Config.endpoints.playlists}?part=snippet&id=${playlistId}&maxResults=1&fields=items%2Fsnippet%2Ftitle&key=${Config.key}`)
             .done((data) => {
                 const title = data.items[0].snippet.title;
 
-                this._region.show(
-                    new BreadcrumbView({
-                        collection: new Collection([
-                            { title: 'Playlists', route: 'playlists' },
-                            { title }
-                        ])
-                    })
-                );
+                // Cache
+                sessionStorage.set(`${playlistId}.info`, { title });
+
+                this._showBreadcrumb([
+                    { title: 'Playlists', route: 'playlists' },
+                    { title }
+                ]);
             });
 
         this._currentPlaylistId = playlistId;
+    }
+
+    _showBreadcrumb(list) {
+        this._region.show(
+            new BreadcrumbView({
+                collection: new Collection(list)
+            })
+        );
     }
 }
 
