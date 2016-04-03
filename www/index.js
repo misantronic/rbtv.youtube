e1a5c5c6210838fc4836491c9c114a1d5979007c
David Schkalee <david.schkalee@sysvision.de>
2016-04-04 00:53:35 +0200

    Added compression (gzip, deflate) to www-env

--- www/index.js
+++ www/index.js
@@ -1,18 +1,21 @@
-var express = require('express');
+var express     = require('express');
+var compression = require('compression');
+
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
+app.use(compression());
 app.use(express.static(__dirname + '/../public'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/../public');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
+app.get('/', function (request, response) {
     response.render('index');
 });
 
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), function () {
     console.log('Node app is running on port', app.get('port'));
 });
