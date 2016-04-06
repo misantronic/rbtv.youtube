427c50b89af0670c07dbcf6629c00d8fc5a5c56b
David Schkalee <david.schkalee@sysvision.de>
2016-04-07 01:43:48 +0200

    Store videos to mongoDB

--- www/api/endpoints/videos.js
+++ www/api/endpoints/videos.js
@@ -1,13 +1,41 @@
-var request = require('./../request');
-var cache   = require('../cache');
+var _          = require('underscore');
+var request    = require('./../request');
+var cache      = require('../cache');
+var db         = require('../db');
+var VideoModel = require('../models/Video');
 
 module.exports = function (req, res) {
     var query = req.query;
 
     var cacheConfig = new cache.Config(
-        'videos.'+ query.id,
+        'videos.' + query.id,
         60 * 60 * 24 * 7 // 7 days
     );
 
-    request(res, 'videos', query, cacheConfig);
+    request(res, 'videos', query, cacheConfig)
+        .done((videoData) => {
+            _.each(videoData.items, function (videoItem) {
+                VideoModel.findById(videoItem.id, function (err, res) {
+                    if (!res) {
+                        var video = new VideoModel({
+                            _id: videoItem.id,
+                            etag: videoItem.etag,
+                            kind: videoItem.kind,
+                            channelId: videoItem.snippet.channelId,
+                            description: videoItem.snippet.description,
+                            publishedAt: videoItem.snippet.publishedAt,
+                            thumbnails: videoItem.snippet.thumbnails,
+                            tags: videoItem.snippet.tags,
+                            title: videoItem.snippet.title
+                        });
+
+                        video.save(function (err) {
+                            if (err) {
+                                console.error('Error trying to save videoItem', err)
+                            }
+                        });
+                    }
+                });
+            });
+        });
 };
---
+++ www/api/models/Video.js
@@ -0,0 +1,42 @@
+var db = require('../db');
+
+var videoSchema = new db.Schema({
+    _id: { type: String, unique: false },
+    kind: String,
+    etag: String,
+    title: String,
+    description: String,
+    tags: [],
+    publishedAt: {
+        type: Date, default: Date.now
+    },
+    thumbnails: {
+        default: {
+            width: Number,
+            heiht: Number,
+            url: String
+        },
+        medium: {
+            width: Number,
+            heiht: Number,
+            url: String
+        },
+        high: {
+            width: Number,
+            heiht: Number,
+            url: String
+        },
+        standard: {
+            width: Number,
+            heiht: Number,
+            url: String
+        },
+        maxres: {
+            width: Number,
+            heiht: Number,
+            url: String
+        }
+    }
+});
+
+module.exports = db.model('Videos', videoSchema);
