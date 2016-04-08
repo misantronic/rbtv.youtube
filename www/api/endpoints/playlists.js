a8e691a97afb3eb9d6d0bb95e2a00a475a035d7d
David Schkalee <david.schkalee@sysvision.de>
2016-04-08 12:14:10 +0200

    Dont persist playlist-requests to redis

--- www/api/endpoints/playlists.js
+++ www/api/endpoints/playlists.js
@@ -10,11 +10,7 @@ module.exports = function (req, res) {
             new request.Config({
                 response: res,
                 endpoint: 'playlists',
-                query: query,
-                cacheConfig: new cache.Config(
-                    cache.rk('playlists', query.channelId, query.pageToken, query.maxResults),
-                    60 * 60 * 24 * 3 // 3 days
-                )
+                query: query
             })
         );
     };
