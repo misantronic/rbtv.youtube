83f6af7c3effff5ae8519c3211afeb6208fe8b42
David Schkalee <david.schkalee@sysvision.de>
2016-05-04 15:27:56 +0200

    Added mongoose logging

--- www/db/db.js
+++ www/db/db.js
@@ -1,14 +1,32 @@
 var mongoose = require("mongoose");
 
-var mongoURI = process.env.MONGOLAB_URI;
+var mongoURI   = process.env.MONGOLAB_URI;
+var connection = mongoose.connection;
 
-if (!mongoose.connection.readyState) {
+if (!connection.readyState) {
     mongoose.connect(mongoURI, function (err, res) {
         if (err) {
             return console.log('Mongo: ERROR connecting to: ' + mongoURI + '. ' + err);
         }
 
         console.log('Mongo: Connected to: ' + mongoURI);
+    });
+
+    connection.on('error', function (error) {
+        console.error('Mongo: Error: ' + error);
+        mongoose.disconnect();
+    });
+
+    connection.once('open', function() {
+        console.log('Mongo: Connection opened...');
+    });
+
+    connection.on('reconnected', function() {
+        console.log('Mongo: Reconnected...');
+    });
+
+    connection.on('disconnected', function() {
+        console.log('Mongo: Disconnected.');
     });
 }
 
