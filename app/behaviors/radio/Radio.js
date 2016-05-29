2f94321be88b9cb4ed3176dfb99e77b8dbcc9b74
misantronic <misantronic@posteo.se>
2016-05-29 03:13:37 +0200

    Fixed channel Removed console.log

--- app/behaviors/radio/Radio.js
+++ app/behaviors/radio/Radio.js
@@ -1,16 +1,18 @@
 import {Behavior} from 'backbone.marionette'
+import channels from '../../channels'
 import _ from 'underscore'
 
 class Radio extends Behavior {
     initialize() {
-        _.each(this.options, (events, channel) => {
+        _.each(this.options, (events, channelStr) => {
+            const channel = channels[channelStr];
+
             _.each(events, (handler, event) => {
                 if (_.isString(handler)) {
                     handler = this.view[handler];
                 }
 
                 this.view.listenTo(channel, event, handler);
-                console.log(this.view, channel, event, handler);
             });
         });
     }
