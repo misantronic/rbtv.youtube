cb4e5120d4ad82da711f2cc99a660e69de8746d2
David Schkalee <david.schkalee@sysvision.de>
2016-06-10 21:07:16 +0200

    Added connection-timeout

--- www/index.js
+++ www/index.js
@@ -1,4 +1,5 @@
 var express     = require('express');
+var timeout     = require('connect-timeout');
 var compression = require('compression');
 var bodyParser  = require('body-parser');
 var api         = require('./api/api');
@@ -9,11 +10,16 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
+app.use(timeout('60s'));
 app.use(compression());
 app.use(express.static(publicPath));
 
 // parse application/json
-app.use(bodyParser.json({limit: '5mb'}));
+app.use(bodyParser.json({ limit: '5mb' }));
+
+app.use(function (req, res, next) {
+    if (!req.timedout) next();
+});
 
 // views is directory for all template files
 app.set('views', publicPath);
