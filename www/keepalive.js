ba9000c679d324d2f8fde2e1737200ddafa33285
David Schkalee <david.schkalee@sysvision.de>
2016-06-12 21:11:21 +0200

    Added log

--- www/keepalive.js
+++ www/keepalive.js
@@ -2,6 +2,8 @@ var http = require('http');
 
 var appUrl = 'http://rbtv-youtube.herokuapp.com';
 
+console.log('Running keepalive...');
+
 setInterval(function () {
     console.log('keepalive: Refreshing app "'+ appUrl +'"...');
     http.get(appUrl);
